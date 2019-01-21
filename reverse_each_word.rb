a2b062c2b6eb96c63ead03aa14b77a9be0569863
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-21 13:49:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(saying)
   new_saying = ""
-  saying.split " "
-  saying.each do |word|
+  new_array = saying.split " "
+  new_array.each do |word|
     new_saying << word.reverse 
   end
   new_saying
